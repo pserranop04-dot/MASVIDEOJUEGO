faf2b885216e4d1dd67f1d1dc8380912e4fd0240
smedina-3 <sarymedina93@gmail.com>
2026-05-18 23:47:20 +0200

    salto 2

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -6,6 +6,8 @@ public class MovPersonaje : MonoBehaviour
 
     public float multiplicador = 5f;
     public float salto = 0.1f;
+
+    private bool puedoSaltar = true;
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,10 +32,23 @@ public class MovPersonaje : MonoBehaviour
 
     //Movimiento personaje
         //rb.linearVelocity = new Vector2(miDeltaTime, rb.linearVelocityY);
+    
+    
+    //Salto
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.magenta);
 
+        if (hit)
+        {
+            puedoSaltar = true;
+            Debug.Log(hit.collider.name);
+        }else
+        {
+            puedoSaltar = false;
+        }
 
     // Salto
-        if (InputSystem.actions["Jump"].IsPressed())
+        if (InputSystem.actions["Jump"].IsPressed() )//&& puedoSaltar)
         {
            rb.AddForce(
             new Vector2 (0,salto),
@@ -42,4 +57,5 @@ public class MovPersonaje : MonoBehaviour
         };
         
     }
+
 }
