4658e2e8813c564971e9320be999b53edb539ba0
pserranop04-dot <pserranop04@gmail.com>
2026-05-27 13:33:17 +0200

    copia a la rama main

--- Assets/Scripts3/Dead.cs
+++ Assets/Scripts3/Dead.cs
@@ -1,30 +1,28 @@
 using UnityEngine;
 
+
 public class Dead : MonoBehaviour
 {
-
     private GameObject personaje;
     private MovPersonaje movPersonaje;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         personaje = GameObject.Find("Personaje");
         movPersonaje = personaje.GetComponent<MovPersonaje>();
-        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.name == "Personaje")
+        if (col.name == "Personaje")
         {
             Debug.Log(col.name);
             movPersonaje.Respawnear();
         }
+        
     }
 }
